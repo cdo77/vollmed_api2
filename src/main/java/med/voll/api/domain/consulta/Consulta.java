8b9aa24ddd0a600f005a30ac369c2d39856cba84
cdo77 <53411877+cdo77@users.noreply.github.com>
2023-09-30 12:37:49 -0300

    VOLLMED_API2 andando

---
+++ src/main/java/med/voll/api/domain/consulta/Consulta.java
@@ -0,0 +1,54 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.paciente.Paciente;
+
+import java.time.LocalDateTime;
+
+@Table(name = "consultas")
+@Entity(name = "Consulta")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Consulta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+
+    private LocalDateTime fecha;
+
+
+    public Consulta(Object o, Medico medico, Paciente paciente, LocalDateTime fecha) {
+    }
+
+    public Long getId() {
+        return null;
+    }
+
+    public Consulta getPaciente() {
+        return null;
+    }
+
+    public Consulta getMedico() {
+        return null;
+    }
+
+    public LocalDateTime getFecha() {
+        return null;
+    }
+}
